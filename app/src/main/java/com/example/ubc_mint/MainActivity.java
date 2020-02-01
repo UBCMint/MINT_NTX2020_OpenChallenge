f4beaf9fed765d64ec6343121474807df9a3be54
stephanie <stephaniechen0630@gmail.com>
2020-02-01 12:16:35 -0800

    added new activity

--- app/src/main/java/com/example/ubc_mint/MainActivity.java
+++ app/src/main/java/com/example/ubc_mint/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.ubc_mint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTextToSpeech();
+            }
+        });
+    }
+
+    public void openTextToSpeech() {
+        Intent intent = new Intent(this, TextToSpeech.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/ubc_mint/TextToSpeech.java
@@ -0,0 +1,14 @@
+package com.example.ubc_mint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextToSpeech extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_to_speech);
+    }
+}
