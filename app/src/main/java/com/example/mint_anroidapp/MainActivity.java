5f874a08bfbcefd108802b6a436aad54e11ca7d9
JoshuaParkSJ <jpark9061@gmail.com>
2020-03-07 14:25:41 -0800

    add_device_page_added

--- app/src/main/java/com/example/mint_anroidapp/MainActivity.java
+++ app/src/main/java/com/example/mint_anroidapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.mint_anroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public ImageView buttonToAddPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonToAddPage = findViewById(R.id.add0);
+        buttonToAddPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, add_page.class);
+                MainActivity.this.startActivity(i);
+            }
+        });
     }
+
 }
+
---
+++ app/src/main/java/com/example/mint_anroidapp/add_page.java
@@ -0,0 +1,32 @@
+package com.example.mint_anroidapp;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class add_page extends AppCompatActivity{
+
+    AnimationDrawable animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_page);
+
+        ImageView loading = (ImageView)findViewById(R.id.loading);
+        animation = (AnimationDrawable)loading.getDrawable();
+    }
+
+    public void start(View v) {
+        animation.start();
+    }
+
+    public void stop(View v) {
+        animation.stop();
+    }
+}
+
