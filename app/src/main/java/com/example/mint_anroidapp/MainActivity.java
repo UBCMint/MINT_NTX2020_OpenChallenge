e96effd45e841a7d541d5908cf4973a9f4788486
Victor Liang <liangvictor3@gmail.com>
2020-11-19 17:20:08 -0800

    Properly import multilayer_conv_178.pt and load it with Pytorch Android

--- app/src/main/java/com/example/mint_anroidapp/MainActivity.java
+++ app/src/main/java/com/example/mint_anroidapp/MainActivity.java
@@ -3,21 +3,19 @@ package com.example.mint_anroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -29,7 +27,14 @@ import com.google.android.material.textfield.TextInputLayout;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -85,6 +90,55 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // PyTorch
+        Bitmap bitmap = null;
+        Module module = null;
+
+        try {
+            // creating bitmap from packaged into app android asset 'image.jpg',
+            // app/src/main/assets/image.jpg
+            bitmap = BitmapFactory.decodeStream(getAssets().open("wolf.jpg"));
+            // loading serialized torchscript module from packaged into app android asset model.pt,
+            // app/src/model/assets/model.pt
+//            module = Module.load(assetFilePath(this, "ResNetModel.pt"));
+            module = Module.load(assetFilePath(this, "xxx_model.pt"));
+
+        } catch (IOException e) {
+            Log.e("PyTorch", "Error reading assets", e);
+            finish();
+        }
+
+//        final Tensor inputTensor = Tensor
+//
+//        // showing image on UI
+//        ImageView imageView = findViewById(R.id.image);
+//        imageView.setImageBitmap(bitmap);
+//
+//        // preparing input tensor
+//        final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
+//                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+//
+//        // running the model
+//        final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+//
+//        // getting tensor content as java array of floats
+//        final float[] scores = outputTensor.getDataAsFloatArray();
+//
+//        // searching for the index with maximum score
+//        float maxScore = -Float.MAX_VALUE;
+//        int maxScoreIdx = -1;
+//        for (int i = 0; i < scores.length; i++) {
+//            if (scores[i] > maxScore) {
+//                maxScore = scores[i];
+//                maxScoreIdx = i;
+//            }
+//        }
+//
+//        String className = ImageNetClasses.IMAGENET_CLASSES[maxScoreIdx];
+//
+//        // showing className on UI
+//        TextView textView = findViewById(R.id.outputText);
+//        textView.setText(className);
     }
 
     public void OpenSettings() {
@@ -300,5 +354,27 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-}
+    /**
+     * Copies specified asset to the file in /files app directory and returns this file absolute path.
+     *
+     * @return absolute file path
+     */
+    public static String assetFilePath(Context context, String assetName) throws IOException {
+        File file = new File(context.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
 
+        try (InputStream is = context.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        }
+    }
+}
