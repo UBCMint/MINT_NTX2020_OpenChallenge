e070e3598bf0c848544650e4606f40a606717d6c
JoshuaParkSJ <jpark9061@gmail.com>
2020-11-20 22:10:17 -0800

    feat: login with auth and storage

---
+++ app/src/main/java/com/example/mint_anroidapp/SecondActivity.java
@@ -0,0 +1,72 @@
+package com.example.mint_anroidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        logout = (Button)findViewById(R.id.btnLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Logout();
+            }
+        });
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+                break;
+            }
+            case R.id.profileMenu:
+                startActivity(new Intent(SecondActivity.this, ProfileActivity.class));
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
